a40e76e880d3df4bad34b7fd9163058a4f0f914e
keepthinkingit <781130764@qq.com>
2018-10-18 22:44:09 +0800

    Finish layout and simple routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.default')
-@section('title', '关于')
+@section('title', '主页')
 
 @section('content')
-    <h1>主页啦</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <div class="lead">
+            你现在看到的是 <a href="example.me"></a>
+        </div>
+        <p>
+            From Now On.
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
