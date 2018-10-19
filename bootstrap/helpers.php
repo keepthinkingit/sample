1906f861986cf30ade635d667064c543d708da28
keepthinkingit <781130764@qq.com>
2018-10-19 18:45:48 +0800

    fix settings error

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -8,15 +8,15 @@
 
 function get_db_config()
 {
-    if (getenv('IS_IN_HEROKU')){
+    if (getenv('IS_IN_HEROKU')) {
         $url = parse_url(getenv("DATABASE_URL"));
 
         return $db_config = [
             'connection' => 'pgsql',
             'host' => $url["host"],
-            'database' => substr($url["path"],1),
-            'username' => $url["user"],
-            'password' => $url["pass"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
         ];
     } else {
         return $db_config = [
